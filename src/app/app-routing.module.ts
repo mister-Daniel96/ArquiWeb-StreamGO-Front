2ae694ecf245521ed2c8b4fe43438b210de0fc75
Juan Ayala <juandanielayalaquinones@gmail.com>
2023-10-31 12:39:19 -0500

    portada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
+import { ViejoComponent } from './components/viejo/viejo.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+path:'home',component:HomeComponent
+  },
+  {
+    path:'nuevo',component:NuevoComponent
+  },
+  {
+    path:'viejo',component:ViejoComponent
+  },
+  {
+    path:'',redirectTo:'home',pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/viejo/viejo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viejo',
+  templateUrl: './viejo.component.html',
+  styleUrls: ['./viejo.component.css']
+})
+export class ViejoComponent {
+
+}
